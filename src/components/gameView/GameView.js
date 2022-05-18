4604bd3173ca02aed2922c9aacc9a67f4f75be48
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-05-18 18:42:27 +0200

    import game data from json file

--- src/components/gameView/GameView.js
+++ src/components/gameView/GameView.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import gameRules from './GameRules.json';
 
-function GameView({ gameRules }) {
+function GameView() {
+  const [gameQuestion, setGameQuestion] = useState('');
+  const [allWords, setAllWords] = useState([]);
+  const [goodWords, setGoodWords] = useState([]);
+
+  useEffect(() => {
+    const chooseGameOption = Math.floor(Math.random() * gameRules.length);
+
+    setGameQuestion(gameRules[chooseGameOption].question);
+    setAllWords(gameRules[chooseGameOption].all_words);
+    setGoodWords(gameRules[chooseGameOption].good_words);
+  }, []);
+
   return (
     <div className='game-view'>
-      <h2>{gameRules.question}</h2>
+      <h2>{gameQuestion}</h2>
       <div className='wordcloud'>
-        {gameRules.all_words.map((word, index) => {
+        {allWords.map((word, index) => {
           return (
             <div className='word-container' key={index}>
               <p className='word'>{word}</p>
