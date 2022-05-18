895215160f90f5bfba5e4a3c09490059eff72e71
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-05-18 18:44:31 +0200

    render StartView component until nickname is submitted

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import StartView from './components/startView/StartView';
+import { useState } from 'react';
 
 function App() {
+  const [nickname, setNickname] = useState('');
+  const [currentlyPlaying, setCurrentlyPlaying] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {!currentlyPlaying && (
+        <StartView
+          nickname={nickname}
+          setNickname={setNickname}
+          setCurrentlyPlaying={setCurrentlyPlaying}
+        />
+      )}
     </div>
   );
 }
