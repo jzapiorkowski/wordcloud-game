3c6a14310fb09ada58cf547d424fa9895d0ca9a3
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-05-20 12:55:40 +0200

    prevent words from overlapping

--- src/utils/GenerateWordPositions.js
+++ src/utils/GenerateWordPositions.js
@@ -2,12 +2,36 @@ function generateWordPositions(wordsCount, maxWidth, maxHeight) {
   const positions = [];
 
   for (let i = 0; i < wordsCount; i++) {
-    const randomRight = Math.random() * maxWidth;
-    const randomBottom = Math.random() * maxHeight;
+    let randomRight = Math.random() * maxWidth;
+    let randomBottom = Math.random() * maxHeight;
+
+    do {
+      randomRight = Math.random() * (maxWidth - 20) + 20;
+      randomBottom = Math.random() * (maxHeight - 10) + 10;
+
+      checkIfPositionIsOkay(randomBottom, randomRight, positions);
+    } while (checkIfPositionIsOkay(randomBottom, randomRight, positions));
+
     positions.push([randomBottom, randomRight]);
   }
 
   return positions;
+}
+
+function checkIfPositionIsOkay(bottom, right, allPositions) {
+  let isCorrect = false;
+
+  allPositions.forEach((currPosition) => {
+    const [currBottom, currRight] = currPosition;
+    if (
+      Math.abs(currBottom - bottom) < 60 &&
+      Math.abs(currRight - right) < 100
+    ) {
+      isCorrect = true;
+    }
+  });
+
+  return isCorrect;
 }
 
 export default generateWordPositions;
